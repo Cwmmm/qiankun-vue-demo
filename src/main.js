346cd784d9d677d766639a5a2257c57c7f0725bd
cwm <cwmmm5277@gmail.com>
2021-09-16 22:15:57 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
 
+//注册子应用
+import "./rigist";
 
 Vue.use(ElementUI);
 
@@ -13,10 +15,3 @@ new Vue({
   router,
   render: (h) => h(App),
 }).$mount("#app");
-
-//注册子应用
-import './rigist'
-
-
-//全局状态管理
-import './store'
--- src/store.js
+++ src/store.js
@@ -1,22 +1,24 @@
 // main/src/store.js
 
-import { initGlobalState } from 'qiankun';
-import Vue from 'vue'
+import { initGlobalState } from "qiankun";
+import Vue from "vue";
 
 //父应用的初始state
 // Vue.observable是为了让initialState变成可响应：https://cn.vuejs.org/v2/api/#Vue-observable。
 let initialState = Vue.observable({
-  user: {},
+  user: {
+    name: "unChangeName",
+  },
 });
 
 const actions = initGlobalState(initialState);
 
 actions.onGlobalStateChange((newState, prev) => {
   // state: 变更后的状态; prev 变更前的状态
-  console.log('main change', JSON.stringify(newState), JSON.stringify(prev));
+  console.log("main change", JSON.stringify(newState), JSON.stringify(prev));
 
   for (let key in newState) {
-    initialState[key] = newState[key]
+    initialState[key] = newState[key];
   }
 });
 
@@ -24,7 +26,7 @@ actions.onGlobalStateChange((newState, prev) => {
 actions.getGlobalState = (key) => {
   // 有key，表示取globalState下的某个子级对象
   // 无key，表示取全部
-  return key ? initialState[key] : initialState
-}
+  return key ? initialState[key] : initialState;
+};
 
 export default actions;
